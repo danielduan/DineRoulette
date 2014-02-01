ff0e8435186e97768f255994ca7b7fd336925075
Daniel Duan <danielduan88@yahoo.com>
2014-02-01 04:08:37 -0800

    yelp search

---
+++ controllers/YelpController.js
@@ -0,0 +1,29 @@
+var yelp = require("yelp").createClient({
+  consumer_key: "YELP_CONSUMER_KEY", 
+  consumer_secret: "YELP_CONSUMER_SECRET",
+  token: "YELP_TOKEN",
+  token_secret: "YELP_TOKEN_SECRET"
+});
+
+//req format:
+//lat: latitude of search area
+//lon: longitude of ..
+//
+exports.getRestaurants = function(req, res) {
+  var lat = req.body.lat;
+  var lon = req.body.lon;
+  yelp.search({term: "restaurants", ll: lat + "," + lon, limit: "20"}, function(error, data) {
+    console.log(error);
+    console.log(data);
+    res.json(data);
+  });
+}
+
+// See http://www.yelp.com/developers/documentation/v2/search_api
+
+
+// See http://www.yelp.com/developers/documentation/v2/business
+// yelp.business("yelp-san-francisco", function(error, data) {
+//   console.log(error);
+//   console.log(data);
+// });
--- routes/MobileRoutes.js
+++ routes/MobileRoutes.js
@@ -1,3 +1,5 @@
+var yelpcontroller = require("../controllers/YelpControllers.js");
+
 exports.getRestaurants = function(req, res) {
-  
+  yelpcontroller.getRestaurants(req,res);
 }
